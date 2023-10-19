c2d9622831841e0a93d9dedccbcf79b57aa547a6
Stella <gowildyj@gmail.com>
2023-10-19 19:49:58 +0900

    second try

---
+++ eureka/src/test/java/org/school/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.school.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
